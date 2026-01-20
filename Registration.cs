2ca0041b9bf059523c4b144bc6d280d44b4f96de
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-20 12:22:34 -0600

    Update Registration.cs

--- Registration.cs
+++ Registration.cs
@@ -3,7 +3,7 @@ public class Registration
 {
     public bool oldEnough = true;
     
-    public int ValidateAge(){
+    public string ValidateAge(){
         
         if(oldEnough){
             return "old enough";
@@ -11,6 +11,7 @@ public class Registration
             return "";
     }   
 }
+
 
 
 
